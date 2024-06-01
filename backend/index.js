6693a34e8077d632ba4175dbecdc588b08fb723f
katlegorphele <katlegophele95@gmail.com>
2024-06-02 01:19:52 +0200

    basic backend server setup

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,24 @@ app.use(express.json());
 dotenv.config();
 
 // Initialize Moralis
+Moralis.start({
+    apiKey: MORALIS_API_KEY,
+}).then(() => {
+    console.log("Listening for API Calls");
+});
+
+// Endpoint
+app.get("/getwalletbalance", async (req, res) => {
+    try {
+        const {query} = req;
+        const response = await Moralis.EvmApi.balance.getNativeBalance({
+            chain: "534351",
+            address: query.address,
+        });
+        return res.status(200).json(response);
+        }
+    catch (error) {
+        console.error(error);
+        return res.status(400).json({ error: error.message });
+    }
+});

2e1b4a0022aa43bd35a712e52869d95accc5bfaa
katlegorphele <katlegophele95@gmail.com>
2024-06-02 07:28:56 +0200

    frontend added - connect wallet functionality not working

--- frontend/components/staking.js
+++ frontend/components/staking.js
@@ -99,6 +99,140 @@ export default function Staking() {
         contract.closePosition(positionId);
     };
 
-    
+    return (
+        <section className={styles.stakingContainer}>
+      <section>
+        <section className={styles.stakeUnstakeTab}>
+          <section
+            className={`${stakingTab ? styles.stakingType : ""}`}
+            id="stake"
+            onClick={switchToStake}
+          >
+            Stake
+          </section>
+          <section
+            className={`${unstakingTab ? styles.stakingType : ""}`}
+            id="unstake"
+            onClick={switchToUnstake}
+          >
+            Unstake
+          </section>
+        </section>
+        <section className={styles.stakingSection}>
+          <span className={styles.apy}>7% APY</span>
+          {stakingTab ? (
+            <section className={styles.stakingBox}>
+              <h2>Stake</h2>
+              <input
+                className={styles.inputField}
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+              <section className={styles.stakingInfo}>
+                <p>
+                  Balance:{" "}
+                  <span>{(walletBalance / 10 ** 18).toLocaleString()}</span>
+                </p>
+                <p>Exchange Rate: 1.03582967</p>
+                {/* <p>Transaction Cost</p> */}
+              </section>
+              <button
+                className={styles.stakeBtn}
+                onClick={() => stakeEther(0, "7%")}
+              >
+                STAKE
+              </button>
+            </section>
+          ) : (
+            <section className={styles.stakingBox}>
+              <h2>Unstake</h2>
+              <input
+                className={styles.inputField}
+                value={unstakeValue}
+                onChange={(e) => setUnstakeValue(e.target.value)}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+              <section className={styles.stakingInfo}>
+                <p>
+                  Balance:{" "}
+                  {assets.length > 0 &&
+                    assets.map((a, id) => {
+                      if (a.open) {
+                        return <span key={id}>{a.etherStaked}</span>;
+                      } else {
+                        return <span></span>;
+                      }
+                    })}
+                </p>
+                {/* <p>Transaction Cost</p> */}
+                <p>
+                  You Receive: {unstakeValue == 0 ? "" : unstakeValue * 1.07}
+                </p>
+              </section>
+              <button
+                className={styles.stakeBtn}
+                onClick={() => withdraw(assets[assets.length - 1].positionId)}
+              >
+                UNSTAKE
+              </button>
+            </section>
+          )}
+        </section>
+      </section>
+      <section>
+        <section className={styles.stakingInfoSection}>
+          <section className={styles.stakingInfo}>
+            <h2>Locked Staking</h2>
+            <section className={styles.lockedStaking}>
+              <span>Locked 30 days</span>
+              <span className={styles.lockedStakingAPY}>8% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
+            <section className={styles.lockedStaking}>
+              <span>Locked 60 days</span>
+              <span className={styles.lockedStakingAPY}>9% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
+            <section className={styles.lockedStaking}>
+              <span>Locked 90 days</span>
+              <span className={styles.lockedStakingAPY}>12% API</span>
+              <input
+                className={styles.inputField}
+                type="number"
+                id="inputField"
+                maxLength="120"
+                placeholder="Enter Amount"
+                required
+              />
+            </section>
+          </section>
+          <button className={styles.stakeBtn}>STAKE</button>
+        </section>
+      </section>
+    </section>
+  );
 
 }
